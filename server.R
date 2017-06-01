890aae04f77d1a8742b9ce99f35d9218331530df
Konrad Pagacz <konrad.pagacz@gmail.com>
2017-06-01 13:43:57 +0200

    progres

--- server.R
+++ server.R
@@ -1,5 +1,20 @@
 library (shiny)
 
 shinyServer(function(input, output, session) {
-  output$directory = renderText(input$fileIn$datapath)
+  
+  output$print = renderTable(file())
+  
+  # output$debug = renderPrint ({
+  #   # str (file()$datapath)
+  #   # paste0(file()$datapath[1], '/', file()$name[1])
+  #   readLines(con = file(file()$datapath[1], encoding = "UTF-8"))
+  # })
+  
+  output$csv = renderTable ({
+    inFile = input$files
+    
+    if (is.null(inFile)) return (NULL)
+    
+    read.csv (inFile$datapath, sep = ';', header = T)
+  })
 })
--- ui.R
+++ ui.R
@@ -1,39 +1,29 @@
 library (shiny)
 
-shinyUI (tagList(fluidPage(
-  includeScript("./text.js"),
-  titlePanel("Glyculator v2 alpha"),
-  fluidRow(
-    numericInput ("idrow", label = "idrow", value = 3),
-    numericInput ("idcol", label = "idcol", value = 2),
-    numericInput ("perday", label = "perday", value = 288),
-    numericInput ("headnrows", label = "headnrows", value = 13),
-    numericInput ("datecol", label = "datecol", value = 2),
-    numericInput ("timecol", label = "timecol", value = 3),
-    numericInput ("dtcol", label = "dtcol", value = 4),
-    numericInput ("glucosecol", label = "glucosecol", value = 10),
-    textInput ("max days", label = "maxdays", value = "F"),
-    textInput ("extension", label = "ext", value = ".csv"),
-    textInput ("separator", label = "sep", value = ","),
-    fileInput ('files', label = 'files', multiple = T)
-  ),
-  
-  # fluidRow(
-  #   wellPanel(
-  #            tags$div(class="form-group shiny-input-container", 
-  #                     tags$div(tags$label("File input")),
-  #                     tags$div(tags$label("Choose folder", class="btn btn-primary",
-  #                                         tags$input(id = "fileIn", webkitdirectory = TRUE, type = "file", style="display: none;", onchange="pressed()"))),
-  #                     tags$label("No folder choosen", id = "noFile"),
-  #                     tags$div(id="fileIn_progress", class="progress progress-striped active shiny-file-input-progress",
-  #                              tags$div(class="progress-bar")
-  #                     )     
-  #            )
-  #   )
-  # ),
-  fluidRow(
-    textOutput ("directory")
+shinyUI (
+    fluidPage(
+      titlePanel("Glyculator v2 alpha"),
+      fluidRow(
+        numericInput ("idrow", label = "idrow", value = 3),
+        numericInput ("idcol", label = "idcol", value = 2),
+        numericInput ("perday", label = "perday", value = 288),
+        numericInput ("headnrows", label = "headnrows", value = 13),
+        numericInput ("datecol", label = "datecol", value = 2),
+        numericInput ("timecol", label = "timecol", value = 3),
+        numericInput ("dtcol", label = "dtcol", value = 4),
+        numericInput ("glucosecol", label = "glucosecol", value = 10),
+        textInput ("max days", label = "maxdays", value = "F"),
+        textInput ("extension", label = "ext", value = ".csv"),
+        textInput ("separator", label = "sep", value = ","),
+        fileInput ('files', label = 'files', multiple = F, accept = c(
+          "text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv"))
+        ),
+      fluidRow(
+        tableOutput ('print'),
+        verbatimTextOutput ('debug'),
+        tableOutput ('csv')
+      )
+      )
   )
-),
-HTML("<script type='text/javascript' src='getFolders.js'></script>")
-))
